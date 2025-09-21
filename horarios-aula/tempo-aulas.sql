215e1a846d888b54ce4b4dc2967e8800875544aa
thiagowaib <thiagowaib@gmail.com>
2025-09-21 16:29:28 -0300

    Query SQL - Tempo em Aulas

---
+++ horarios-aula/tempo-aulas.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.id AS professor_id,
+    p.name AS professor_name,
+    SUM(TIMESTAMPDIFF(HOUR, cs.start_time, cs.end_time)) AS total_hours
+FROM PROFESSOR p
+JOIN SUBJECT s ON p.id = s.taught_by
+JOIN CLASS c ON s.id = c.subject_id
+JOIN CLASS_SCHEDULE cs ON c.id = cs.class_id
+GROUP BY p.id, p.name;
