86e23458446056eee10421731b1b7027efb2997c
thiagowaib <thiagowaib@gmail.com>
2025-09-21 16:37:51 -0300

    Lista Salas Horarios Livres

---
+++ horarios-aula/lista-salas-horarios-livres.sql
@@ -0,0 +1,14 @@
+SELECT 
+    r.id AS room_id,
+    r.name AS room_name,
+    cs.day_of_week,
+    cs.start_time,
+    cs.end_time
+FROM 
+    ROOM r
+LEFT JOIN 
+    CLASS_SCHEDULE cs ON cs.room_id = r.id
+ORDER BY 
+    r.id, cs.day_of_week, cs.start_time;
+
+-- SGBD definido para o SQL: PostgreSQL
--- horarios-aula/tempo-aulas.sql
+++ horarios-aula/tempo-aulas.sql
@@ -7,3 +7,5 @@ JOIN SUBJECT s ON p.id = s.taught_by
 JOIN CLASS c ON s.id = c.subject_id
 JOIN CLASS_SCHEDULE cs ON c.id = cs.class_id
 GROUP BY p.id, p.name;
+
+-- SGBD definido para o SQL: PostgreSQL
